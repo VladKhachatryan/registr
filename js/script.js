bccbc872ca97199f00bf51b1a20cff19e852834b
User <xachatryavlad@gmail.com>
2023-10-30 22:51:13 +0400

    Added Validation

--- js/script.js
+++ js/script.js
@@ -20,21 +20,29 @@ let conf_pass = document.querySelector("#confpass");
 let err_msg = document.querySelector("#error_msg");
 let login = document.querySelector("#login");
 let phone = document.querySelector("#ph_num");
+let smile = document.querySelector("#sml_login");
 
 login.addEventListener("keydown", () => {
-  if (login.value.length <= 3) {
-    err_msg.innerHTML = "Login must be at least 3 letters";
+  if (login.value.length <= 1) {
+    smile.innerHTML = "Login must be at least 3 letters";
+    login.style.border = "2px solid red";
+    smile.style.color = "red";
+  } else if (login.value.length >= 1) {
+    login.style.border = "2px solid green";
+    smile.innerHTML = "";
   } else {
-    err_msg.innerHTML = "";
+    smile.innerHTML = "";
+    login.style.border = ""; // Reset the border to its original state
   }
 });
+// reg_btn.addEventListener("click", () => {
+//   if (!(pass.value === conf_pass.value)) {
+//     err_msg.innerHTML = "Password doesn't match";
+//   } else {
+//     err_msg.innerHTML = "";
+//   }
 
-reg_btn.addEventListener("click", () => {
-  if (!(pass.value === conf_pass.value)) {
-    err_msg.innerHTML = "Password doesn't match";
-  } else {
-    err_msg.innerHTML = "";
-  }
+//   console.log(login.value.length);
+// });
 
-  console.log(login.value.length);
-});
+console.log(login.value.length);
